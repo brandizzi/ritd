7bfcf71520588ea9bcf6423800ecf7f29de43e19
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2016-04-03 23:08:01 -0300

    #2 Fill element by css class.

--- ritd/document.py
+++ ritd/document.py
@@ -18,21 +18,30 @@
 # along with Rain in the Desert. If not, see <http://www.gnu.org/licenses/>.
 
 from xml.etree.ElementTree import fromstring, tostring
-
+from collections import defaultdict
 
 class Document(object):
 
     def __init__(self, content):
         self.content = content
         self.id_map = {}
+        self.class_map = defaultdict(list)
         self.tree = fromstring(content)
         for e in self.tree.iter():
             eid = e.attrib.get('id')
             if eid:
                 self.id_map[eid] = e
 
-    def fill(self, value, cssId=None):
-        self.id_map[id].text = value
+            eclass = e.attrib.get('class')
+            if eclass:
+                self.class_map[eclass].append(e)
+
+    def fill(self, value, cssId=None, cssClass=None):
+        if cssId is not None:
+            self.id_map[cssId].text = value
+        if cssClass is not None:
+            for e in self.class_map[cssClass]:
+                e.text = value
 
     def tostring(self):
         return tostring(self.tree)
